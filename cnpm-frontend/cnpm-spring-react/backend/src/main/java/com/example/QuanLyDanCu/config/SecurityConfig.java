05c89192f0c8366495893b058f44bcfaa3ebe00c
thaimeo111.laptopasus <tranducthai11a@gmail.com>
2025-11-17 22:23:46 +0700

    add CORS Config in SecurityConfig

--- cnpm-frontend/cnpm-spring-react/backend/src/main/java/com/example/QuanLyDanCu/config/SecurityConfig.java
+++ cnpm-frontend/cnpm-spring-react/backend/src/main/java/com/example/QuanLyDanCu/config/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.*;
 import org.springframework.security.web.*;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
@@ -28,12 +32,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         // Swagger UI endpoints
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/api/hokhau/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_TOTRUONG","ROLE_KETOAN")
-                        .requestMatchers("/api/dot-thu-phi/**", "/api/thu-phi-ho-khau/**").hasAnyAuthority("ROLE_ADMIN", "ROLE_TOTRUONG","ROLE_KETOAN")
+                        .requestMatchers("/api/hokhau/**").hasAnyAuthority("ADMIN", "TOTRUONG","KETOAN")
+                        .requestMatchers("/api/dot-thu-phi/**", "/api/thu-phi-ho-khau/**").hasAnyAuthority("ADMIN", "TOTRUONG","KETOAN")
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
@@ -41,5 +46,18 @@ public class SecurityConfig {
         http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
